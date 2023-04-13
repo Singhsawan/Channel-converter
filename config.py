619dd22f4f91a8208dca17538908297d5a619377
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-04-13 07:46:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ def is_enabled(value, default):
 # Mandatory variables for the bot to start
 API_ID = int(os.environ.get("API_ID", "16445683"))
 API_HASH = os.environ.get("API_HASH", "d0852e13eee2389ff2d9183b00649547")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6197312036:AAGLcjgunbVYqe1a-LyOkraK_wTWJm9rYaU")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6264339891:AAHOaAyEFg3s4Rbl62adJtc3E9GfRg5TsNQ")
 DROPLINK_API = os.environ.get("DROPLINK_API")
-MDISK_API = os.environ.get("MDISK_API", "WS15eJdTOMlQzyQ7W3Pp")
+MDISK_API = os.environ.get("MDISK_API", "aECk7a3hAoWVdkN8Zctq")
 ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Sawansingh24")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Sawansingh24:Sawansingh24@cluster0.uiuhxxj.mongodb.net/?retryWrites=true&w=majority")
@@ -23,12 +23,28 @@ WEBSITE = os.environ.get('WEBSITE')
 INCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("INCLUDE_DOMAIN").split(",")) if os.environ.get("INCLUDE_DOMAIN") else []
 EXCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("EXCLUDE_DOMAIN").split(",")) if os.environ.get("EXCLUDE_DOMAIN") else []
 CHANNELS = is_enabled((os.environ.get('CHANNELS', "True")), True)
-CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001535830032")) if os.environ.get("CHANNEL_ID") else []
+CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001902623720")) if os.environ.get("CHANNEL_ID") else []
 FORWARD_MESSAGE = is_enabled((os.environ.get('FORWARD_MESSAGE', "True")), True)
 SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/Sawansingh24/Channel-Mdisk-Converter")
 USERNAME = os.environ.get("USERNAME", "cw_backup_01")
 HEADER_TEXT = os.environ.get("HEADER_TEXT", '')
-FOOTER_TEXT = os.environ.get("FOOTER_TEXT", '')
+FOOTER_TEXT = os.environ.get("FOOTER_TEXT", '▬▬▬▬▬▬▬▬▬▬
+
+📌 𝘼𝙙𝙪𝙡𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡 
+https://t.me/adult_backup001/3
+
+Join our group-
+https://t.me/b_t_movies
+
+Get latest deals on this channel https://t.me/dealsbazzarr
+▬▬▬▬▬▬▬▬▬▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+🚦 HOW TO WATCH & DOWNLOAD 🚦  
+https://t.me/how_to_download_channel/4
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+Share and support ❤️')
 BANNER_IMAGE = os.environ.get("BANNER_IMAGE", '')
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
 LINK_BYPASS = is_enabled((os.environ.get('LINK_BYPASS', "False")), False)
